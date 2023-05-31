c70019ec26b0d96293a7bcae9ba51a71ce44b44b
Emanwhibi <whibieman@gmail.com>
2023-05-31 07:37:05 +0000

    add edit

--- app.js
+++ app.js
@@ -5,14 +5,49 @@ function Food(foodName ,type , price){
     this.foodName = foodName;
     this.type = type;
     this.price = price;
-   menu.push(this)  
+   menu.push(this);  
+}
+
+function genrateRandom() {
+    return Math.floor(Math.random() * (1000-5) + 1);
 };
 
-function genrateRandom(min, max) {
-    return Math.floor(Math.random() * (max-min+1) + min);
-};
-Food.prototype.calculateprice =function(min , max) {
-    this.price = genrateRandom(min , max);
-    console.log(this.price);
+//Render 
+Food.prototype.render = function () {
+    let foodTable = document.getElementById('food');
+
+    let foodRow = document.createElement("tr");
+
+    let foodID  = document.createElement('td')
+    foodId.textContent = genrateRandomID();
+    foodRow.appendChild(foodId);
+
+    let foodName  = document.createElement('td')
+    foodName.textContent = this.foodName;
+    food.appendChild(foodName);
+
+    let foodType = document.createElement('td')
+    foodType.textContent = this.type;
+    foodRow.appendChild("foodType");
+
+    let foodPrice = document.createElement('td');
+   foodPrice.textContent=this.price+"$";
+    foodRow.appendChild(foodPrice);
+
+    foodTable.appendChild(foodRow);
 };
 
+function handler(e){
+    e.preventDefault();
+    let foodName = e.target.foodName.value;
+    let type = e.target.type.value;
+    let Price = e.target.price.value;
+    console.log(foodName,type,price);
+
+    let food = new food(foodName, type , price);
+    console.log(food);
+    food.render();
+}
+
+let saveValues = document.getElementById("foodForm");
+saveValues.addEventListener("submit",handler);
