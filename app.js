018c56671105124ca1f1fb367c77359f8cf5e123
Emanwhibi <whibieman@gmail.com>
2023-06-03 20:06:52 +0000

    add new branch

--- app.js
+++ app.js
@@ -1,53 +1,37 @@
 'use strict';
-let menu =[]
-//Constructor
-function Food(foodName ,type , price){
+
+function Food( id , foodName ,type , price){
+    this.id = id;
     this.foodName = foodName;
     this.type = type;
     this.price = price;
-   menu.push(this);  
-};
-
-function genrateRandom() {
-    return Math.floor(Math.random() * (1000-5) + 1);
-};
-
-//Render 
-Food.prototype.render = function () {
-    let foodTable = document.getElementById('food');
-
-    let foodRow = document.createElement("tr");
-
-    let foodID  = document.createElement('td')
-    foodId.textContent = genrateRandomID();
-    foodRow.appendChild(foodId);
-
-    let foodName  = document.createElement('td')
-    foodName.textContent = this.foodName;
-    food.appendChild(foodName);
-
-    let foodType = document.createElement('td')
-    foodType.textContent = this.type;
-    foodRow.appendChild("foodType");
-
-    let foodPrice = document.createElement('td');
-   foodPrice.textContent=this.price+"$";
-    foodRow.appendChild(foodPrice);
-
-    foodTable.appendChild(foodRow);
-};
-
-function handler(e){
-    e.preventDefault();
-    let foodName = e.target.foodName.value;
-    let type = e.target.type.value;
-    let price = e.target.price.value;
-    console.log(foodName,type,price);
-
-    let food = new food(foodName, type , price);
-    console.log(food);
-    food.render();
+  
 }
 
-let saveValues = document.getElementById("foodForm");
-saveValues.addEventListener("submit",handler);
+Food.prototype.generateId = function () {
+    let randomNumber = Math.floor(Math.random() * 7000) + 1000;
+    return randomNumber;
+   
+};
+
+document.getElementById('foodForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const localData = JSON.parse(localStorage.getItem('foodData')) || [];
+    const foodId = new Food().generateId();
+    const foodName = event.target.foodName.value;
+    const foodType = document.getElementById('foodType').value;
+    const foodPrice = parseFloat(document.getElementById('price').value);
+    const food = new Food(foodId, foodName, foodType, foodPrice);
+
+    localData.push(food);
+    localStorage.setItem('foodData', JSON.stringify(localData)); 
+
+    event.target.foodName.value = '';
+    event.target.foodPrice.value = '';
+});
+
+document.getElementById('clearButton').addEventListener('click', function (event) {
+    event.preventDefault();
+    localStorage.removeItem('foodData');
+});
